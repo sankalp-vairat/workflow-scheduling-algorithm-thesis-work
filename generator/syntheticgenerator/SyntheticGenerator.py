86f36b27d5b3af40f134836bc8eba744f0a706b3
sankalp <vairat.sankalp@gmail.com>
2017-03-25 00:31:28 +0530

    hhh

--- generator/syntheticgenerator/SyntheticGenerator.py
+++ generator/syntheticgenerator/SyntheticGenerator.py
@@ -3,10 +3,26 @@ Created on 23-Mar-2017
 
 @author: sankalp
 '''
+import xml.etree.ElementTree as ET
+
 class SyntheticGenerator():
-    def __init__(self):
-        pass
+    def __init__(self,fileName):
+        self.fileName = fileName
     
     
-    def generate(self):
-        pass
+    def generateSyntheticWorkFlow(self):
+        workFlowName = self.fileName
+        
+        tree = ET.parse('CyberShake_30.xml')
+        root = tree.getroot()
+        rootXmlns = root.get('xmlns')
+        
+        for i in range(0,N):
+            tout = File("task_%d_out.dat"%i, size=sizeDist()*KB)
+            t = Task(id="task_%d"%i, namespace="rand", name="Task", runtime=runtimeDist()*SECONDS, MI=miDist(),storage = sizeDist()*KB,outputs=[tout])
+            w.addJob(t)
+            
+        for job in root.findall('{http://pegasus.isi.edu/schema/DAX}job'):
+            inputs = job.findall('{http://pegasus.isi.edu/schema/DAX}uses')
+            for input in inputs:
+                print input.get('file'),'-->',input.get('size')
